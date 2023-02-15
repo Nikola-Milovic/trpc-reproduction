218aa29f72678044f23051b534848cbcf37ae8ee
Nikola M <nikolamilovic2001@gmail.com>
2023-02-15 11:16:20 +0100

    query fix

--- apps/site/src/pages/index.tsx
+++ apps/site/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { trpc } from "@/trpc";
 
 export default function Proposals() {
-  console.log(trpc.hello.useQuery("hello").data)
+  console.log(trpc.hello.useQuery().data)
   return (
    <div>Hello</div> 
   );
